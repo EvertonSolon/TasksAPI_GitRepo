b88ac720aa93a3320793009b5114f4c39862371d
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-01-28 14:31:34 -0300

    Criação do projeto, da model e dto.

---
+++ TasksAPI/TasksAPI/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateHour { get; set; }
+        public string Local { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
