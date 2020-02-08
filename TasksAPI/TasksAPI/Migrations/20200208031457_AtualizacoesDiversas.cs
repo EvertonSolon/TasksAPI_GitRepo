3a8b5392cd6f9e6178854f1a7621c69febc702f4
Everton Solon <EvertonSolon@users.noreply.github.com>
2020-02-08 00:17:30 -0300

    Adicionado novo migration com atualização do nome da propriedade.

---
+++ TasksAPI/TasksAPI/Migrations/20200208031457_AtualizacoesDiversas.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TasksAPI.Migrations
+{
+    public partial class AtualizacoesDiversas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserFullName",
+                table: "AspNetUsers",
+                newName: "FullName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "AspNetUsers",
+                newName: "UserFullName");
+        }
+    }
+}
